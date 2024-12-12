5cffaf3091cf204221e7c30172b74cc7f215e166
qhfan07 <qhfan07@gmail.com>
2024-12-12 15:26:46 -0800

    third commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,17 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const originalFetch = window.fetch;
+
+window.fetch = async (input, init) => {
+  if (typeof input === "string" && input.startsWith("/")) {
+    input = `${import.meta.env.VITE_API_BASE_URL}${input}`;
+  }
+
+  return originalFetch(input, init);
+};
+
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
